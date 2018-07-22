005cd494a20d38913e73786356e4c02ce4b6d183
Victor <Victor@LAPTOP-369FAMDO>
2018-07-22 19:44:36 -0300

    Commit inicial para o meu projeto jsf primefaces

---
+++ projetocompletojsfprimefaces/src/br/com/framework/hibernate/session/HibernateUtil.java
@@ -0,0 +1,119 @@
+package br.com.framework.hibernate.session;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.SessionFactoryImplementor;
+
+import br.com.framework.implementacao.crud.VariavelConexaoUtil;
+
+/**
+ * 
+ * @author Victor Responsável por estabelecer a conexao com o Hibernate
+ */
+@ApplicationScoped
+public class HibernateUtil implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	/**
+	 * Responsável por ler o arquivo de configuração do hibernate.cfg.xml
+	 * 
+	 * @return SessionFactory
+	 */
+	@SuppressWarnings("deprecation")
+	private static SessionFactory buildSessionFactory() {
+
+		try {
+			if (sessionFactory == null) {
+				sessionFactory = new Configuration().configure().buildSessionFactory();
+			}
+			return sessionFactory;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ExceptionInInitializerError("Erro ao criar conexão SessionFactory");
+		}
+	}
+
+	/**
+	 * Retorna o SessionFactory corrente
+	 * 
+	 * @return SessionFactory
+	 */
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * Retorna a sessão do SessionFactory
+	 * 
+	 * @return Session
+	 */
+	public static Session getCurrentSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+
+	/**
+	 * Abre uma nova sessão no SessionFactory
+	 * 
+	 * @return Session
+	 */
+
+	public static Session openSession() {
+		if (sessionFactory == null) {
+			buildSessionFactory();
+		}
+		return sessionFactory.openSession();
+	}
+
+	/**
+	 * Obtem a connection do provedor de conexões configurado
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+
+	public static Connection getConnectionProvider() throws SQLException {
+		return ((SessionFactoryImplementor) sessionFactory).getConnectionProvider().getConnection();
+	}
+
+	/**
+	 * 
+	 * @return Connection no InitialContext java:/comp/env/jdbc/datasource
+	 * @throws SQLException
+	 * @throws NamingException
+	 */
+	public static Connection getConnection() throws SQLException, NamingException {
+		InitialContext context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup(JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+		return ds.getConnection();
+	}
+	
+	/**
+	 * 
+	 * @return DataSourceJndi
+	 * @throws NamingException
+	 */
+	
+	public DataSource getDataSourceJndi() throws NamingException {
+		
+		InitialContext context = new InitialContext();
+		return (DataSource) context.lookup(VariavelConexaoUtil.JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+	}
+}
---
+++ projetocompletojsfprimefaces/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,12 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * 
+ * @author Victor
+ *  Nome do caminho do JNDI datasource Tomcat
+ */
+
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE ="java:/comp/env/jdbc/datasource";
+}
